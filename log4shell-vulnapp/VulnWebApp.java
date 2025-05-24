caf9635824b6cf5fb3d3a107e88562e11ef4e554
Howie Howerton <howieh@sentinelone.com>
2025-05-24 17:22:53 -0400

    back to basics

--- log4shell-vulnapp/VulnWebApp.java
+++ log4shell-vulnapp/VulnWebApp.java
@@ -1,20 +1,30 @@
+import java.io.*;
+import java.net.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
-import java.io.IOException;
-import org.apache.logging.log4j.*;
-
-public class VulnWebApp extends HttpServlet {
+public class VulnWebApp {
     private static final Logger logger = LogManager.getLogger(VulnWebApp.class);
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String name = request.getParameter("name");
-        if (name == null) name = "world";
-        logger.info("Received request from " + name);
-        response.setContentType("text/html");
-        response.getWriter().println("<form method='get'>" +
-            "Name: <input type='text' name='name'/>" +
-            "<input type='submit' value='OK'/>" +
-            "</form><p>Hello, " + name + "!</p>");
+    public static void main(String[] args) throws Exception {
+        ServerSocket serverSocket = new ServerSocket(8080);
+        System.out.println("Vulnerable Log4j web server listening on port 8080...");
+
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            String line;
+            StringBuilder request = new StringBuilder();
+            while ((line = in.readLine()) != null && !line.isEmpty()) {
+                request.append(line).append("\n");
+            }
+
+            logger.error("Received request:\n" + request.toString());
+
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+            out.write("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nHello from vulnerable server\n");
+            out.flush();
+            clientSocket.close();
+        }
     }
 }
