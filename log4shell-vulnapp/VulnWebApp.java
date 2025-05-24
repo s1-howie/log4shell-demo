42b04f79da8726ded6891ed5e880587e0d9cf5f7
Howie Howerton <howieh@sentinelone.com>
2025-05-24 14:11:36 -0400

    update

--- log4shell-vulnapp/VulnWebApp.java
+++ log4shell-vulnapp/VulnWebApp.java
@@ -1,25 +1,18 @@
-
 import javax.servlet.*;
 import javax.servlet.http.*;
-import java.io.*;
-import org.apache.logging.log4j.*;
+import java.io.IOException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class VulnWebApp extends HttpServlet {
     private static final Logger logger = LogManager.getLogger(VulnWebApp.class);
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException, IOException {
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>Log4Shell Test App</h1>");
-        out.println("<form method='POST'><input name='userInput' type='text'/><input type='submit'/></form>");
-        out.println("</body></html>");
-    }
-
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException, IOException {
-        String userInput = request.getParameter("userInput");
-        logger.error("User input: " + userInput);
-        response.getWriter().println("Input received: " + userInput);
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String user = request.getParameter("user");
+        logger.info("User input: " + user);  // vulnerable line for Log4Shell
+        response.setContentType("text/html");
+        response.getWriter().println("<html><body><form method='GET'>" +
+            "Username: <input name='user'/> <input type='submit'/>" +
+            "</form></body></html>");
     }
 }
