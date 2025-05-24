0d2342a9ed2aabd81e15853000e68d0388a7d54a
Howie Howerton <howieh@sentinelone.com>
2025-05-24 14:58:20 -0400

    4

--- log4shell-vulnapp/VulnWebApp.java
+++ log4shell-vulnapp/VulnWebApp.java
@@ -1,25 +1,19 @@
 
 import javax.servlet.*;
 import javax.servlet.http.*;
-import java.io.*;
-import org.apache.logging.log4j.*;
+import java.io.IOException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class VulnWebApp extends HttpServlet {
-    private static final Logger logger = LogManager.getLogger(VulnWebApp.class);
+    static Logger logger = LogManager.getLogger(VulnWebApp.class);
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String user = request.getParameter("user");
-        if (user != null) {
-            logger.info("User input: " + user);
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("name");
+        if (name != null) {
+            logger.info("User input: " + name);
         }
-
         response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<form method='GET'>");
-        out.println("Enter your name: <input type='text' name='user' />");
-        out.println("<input type='submit' value='OK' />");
-        out.println("</form>");
-        out.println("</body></html>");
+        response.getWriter().println("<html><body><form method='GET'>Name: <input name='name'/><input type='submit'/></form></body></html>");
     }
 }
