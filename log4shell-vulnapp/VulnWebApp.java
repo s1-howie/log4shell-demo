39bd52745b0a0aed41ff50a8c53c1a89484ebb67
Howie Howerton <howieh@sentinelone.com>
2025-05-24 14:40:00 -0400

    jetty

--- log4shell-vulnapp/VulnWebApp.java
+++ log4shell-vulnapp/VulnWebApp.java
@@ -1,22 +1,30 @@
 import javax.servlet.http.*;
-import javax.servlet.annotation.WebServlet;
+import javax.servlet.annotation.*;
 import java.io.*;
 
-@WebServlet("/")
+@WebServlet(name = "VulnWebApp", urlPatterns = {"/"})
 public class VulnWebApp extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
+        out.println("<html><head><title>Log4Shell PoC</title></head><body>");
+        out.println("<h2>Enter your name:</h2>");
         out.println("<form method='POST'>");
-        out.println("Name: <input type='text' name='name'><br>");
-        out.println("<input type='submit' value='OK'>");
+        out.println("<input type='text' name='name' />");
+        out.println("<input type='submit' value='Submit' />");
         out.println("</form>");
+        out.println("</body></html>");
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
         String name = request.getParameter("name");
+        org.apache.logging.log4j.LogManager.getLogger(VulnWebApp.class).error("User input: " + name);
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-        out.println("Hello, " + name);
+        out.println("<html><head><title>Thanks</title></head><body>");
+        out.println("<h2>Thanks, " + name + "!</h2>");
+        out.println("</body></html>");
     }
 }
